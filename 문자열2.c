9f81aea6085243321be7a5b171195c64dba0f1e6
jaegyeom <asbsds123@gmail.com>
2019-12-21 11:58:22 +0900

    len, cmp
    len cmp 구현

---
+++ 문자열2.c
@@ -0,0 +1,32 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+int strlength(char* str)
+{
+	int len=0;
+	for (int i = 0; str[i] != '\0' ; i++) {
+		len++;
+	}
+	return len;
+}
+
+int strcmp1(char* str1,char* str2)
+{
+	int len = strlength(str1);
+	int len2 = strlength(str2);
+	if (len - len2)return 1;
+	for (int i = 0; i < len; i++) {
+		if (str1[i] != str2[i]) return 1;
+	}
+	return 0;
+}
+
+int main()
+{
+	char str1[100],str2[100];
+	memset(str1, 0, sizeof(str1));
+	memset(str2, 0, sizeof(str2));
+	scanf("%s", str1);
+	scanf("%s", str2);
+	printf("%d",strcmp1(str1,str2));
+	return 0;
+}
